3b0ca455923107a8b96f222b4d79da3cdc4e633c
huxley333 <huxleykievit535@gmail.com>
2026-06-24 16:36:44 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
-document.getElementById("joinBtn").addEventListener("click", () => {
-    alert("Welcome to The Spaghetti SMP!");
-});
+function showTab(tabId) {
+    const tabs = document.querySelectorAll(".tab-content");
+    const buttons = document.querySelectorAll(".tab");
+
+    tabs.forEach(t => t.classList.remove("active"));
+    buttons.forEach(b => b.classList.remove("active"));
+
+    document.getElementById(tabId).classList.add("active");
+
+    event.target.classList.add("active");
+}
